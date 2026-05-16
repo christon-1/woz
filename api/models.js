8e661a784f76c396ac02d36426cfcd08616fbd4c
Rina Wulandari <christonpoh10@gmail.com>
2026-05-16 18:24:54 +0800

    Upload 20 files via GitHub Uploader by Mubashir Hassan

---
+++ api/models.js
@@ -0,0 +1,28 @@
+const { authenticateRequest } = require('./middleware/auth');
+const { getAvailableModels, getModelPrice } = require('./middleware/modelRestriction');
+const { loadDB } = require('./db/database');
+
+module.exports = async (req, res) => {
+  const auth = authenticateRequest(req);
+  if (!auth.authenticated) {
+    return res.status(auth.status).json({ error: auth.error });
+  }
+
+  const db = loadDB();
+  const availableModels = getAvailableModels(auth.apiKey);
+
+  const models = availableModels.map(id => ({
+    id,
+    object: 'model',
+    created: 0,
+    owned_by: (db.creditPrices && db.creditPrices[id]) ? 'gated' : 'unknown',
+    price: getModelPrice(id),
+    available: true
+  }));
+
+  return res.status(200).json({
+    object: 'list',
+    data: models,
+    tier: auth.tier
+  });
+};
